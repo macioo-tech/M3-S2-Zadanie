e4554fd3d950d1df79aa6a88782354ae80fedc0c
mac <orno.morsoft@gmail.com>
2025-10-26 20:27:08 +0100

    package.json initialized

--- backend/index.ts
+++ backend/index.ts
@@ -4,8 +4,6 @@ const PORT = 3000;
 const server = createServer(async (req, res) => {
   res.end(JSON.stringify({ status: 'ok'}))
 
-  // 1. Obsługa endpointów
-  // 2. Proste serwowanie plików statycznych z katalogu frontend (np. pod ścieżką /static/)
 });
 
 server.listen(PORT, () => {
