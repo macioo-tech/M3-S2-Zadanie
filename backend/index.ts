569bcd302107e38615e9fae5cb106e68338227e5
mac <orno.morsoft@gmail.com>
2025-10-30 00:58:11 +0100

    login refactor#2

--- backend/index.ts
+++ backend/index.ts
@@ -12,12 +12,13 @@ const logger = (req : http.IncomingMessage, res : http.ServerResponse, next : Fu
 const jsonMiddleware = (req : http.IncomingMessage, res : http.ServerResponse, next : Function) : void => {
     const url = new URL(req.url!, `http://${req.headers.host}`)
     const [reqPath] = url.pathname.split("/").filter(Boolean);
-    let appType : string = 'application/json';
+    let appType : string;
     switch (path.extname(reqPath === undefined ? '/index.html' : reqPath).toLowerCase()) {
         case '.html': appType = 'text/html'; break;
         case '.css': appType = 'text/css'; break;
         case '.js': appType = 'application/javascript'; break;
         case '.json': appType = 'application/json'; break;
+        case '' : appType = 'application/json'; break;
         default: appType = 'text/event-stream';
     }
     res.setHeader('Content-Type', appType)
