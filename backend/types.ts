b79037e85ebe12537dcef374d8601495f2377315
mac <orno.morsoft@gmail.com>
2025-11-09 13:28:24 +0100

    refactored Update in db.ts

--- backend/types.ts
+++ backend/types.ts
@@ -1,24 +1,29 @@
 export interface User {
-    id: string;
-    username: string;
-    password: string; // Dla uproszczenia przechowujemy hasło w postaci jawnej (w praktyce należy stosować hashowanie)
-    role: 'admin' | 'user';
-    balance: number;
-  }
-  
-  export interface Car {
-    id: string;
-    model: string;
-    price: number;
-    ownerId: string;
-  }
+  id: string;
+  username: string;
+  password: string; // Dla uproszczenia przechowujemy hasło w postaci jawnej (w praktyce należy stosować hashowanie)
+  role: 'admin' | 'user';
+  balance: number;
+}
+
+export interface Car {
+  id: string;
+  model: string;
+  price: number;
+  ownerId: string;
+}
 
 export type TypeMap = {
-    users: User;
-    cars: Car;
+  users: User;
+  cars: Car;
 };
 
 export interface TokenPayload {
-    userId: string;
-    exp?: number;
+  userId: string;
+  exp?: number;
+}
+
+export interface JSONResponse<T extends keyof TypeMap> {
+  data?: TypeMap[T][],
+  message?: string,
 }
