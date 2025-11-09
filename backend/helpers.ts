f297312176b98d5fe98ae6704e21bee72b25ae20
mac <orno.morsoft@gmail.com>
2025-11-09 13:28:33 +0100

    refactored Update in db.ts

--- backend/helpers.ts
+++ backend/helpers.ts
@@ -1,9 +1,10 @@
 import path from 'node:path';
-import {IncomingMessage, ServerResponse} from 'node:http';
-import {authUser} from './auth.js';
+import { IncomingMessage, ServerResponse } from 'node:http';
+import { authUser } from './auth.js';
+import { JSONResponse, TypeMap } from './types.js';
 
-export function findHeader(reqPath: string): string {
-  switch (path.extname(reqPath === undefined ? '/index.html' : reqPath).toLowerCase()) {
+export function findHeader( reqPath: string ): string {
+  switch ( path.extname( reqPath === undefined ? '/index.html' : reqPath ).toLowerCase() ) {
     case '.html':
       return 'text/html';
     case '.css':
@@ -15,48 +16,48 @@ export function findHeader(reqPath: string): string {
   }
 }
 
-export async function parseBody(req: IncomingMessage): Promise<string> {
-  return new Promise((resolve) => {
+export async function parseBody( req: IncomingMessage ): Promise<string> {
+  return new Promise( ( resolve ) => {
     let data = '';
-    req.on('data', (chunk) => {
+    req.on( 'data', ( chunk ) => {
       data += chunk;
-    });
-    req.on('end', () => {
-      resolve(data);
-    });
-  });
+    } );
+    req.on( 'end', () => {
+      resolve( data );
+    } );
+  } );
 }
 
-export async function checkAuth(req: IncomingMessage, res: ServerResponse): Promise<boolean> {
-  const auth = await authUser(req)
-  if (!auth) {
-    sendJSON(res, 400, {
+export async function checkAuth( req: IncomingMessage, res: ServerResponse ): Promise<boolean> {
+  const auth = await authUser( req )
+  if ( !auth ) {
+    sendJSON( res, 400, {
       error: '❌ User Not Authenticated',
-    })
+    } )
     return false;
   }
   return true;
 }
 
-export function sendJSON(res: ServerResponse, statusCode: number, data: Object) {
-  res.writeHead(statusCode, {'Content-Type': 'application/json'});
-  res.end(JSON.stringify(data));
+export function sendJSON<T extends keyof TypeMap>( res: ServerResponse, statusCode: number, data: JSONResponse<T> ) {
+  res.writeHead( statusCode, { 'Content-Type': 'application/json' } );
+  res.end( JSON.stringify( data ) );
 }
 
-export function sseEvent(event: string, data: object): void {
-  const message = `data: ${JSON.stringify({event, ...data})}\n\n`;
-  clients.forEach((client, index) => {
+export function sseEvent( event: string, data: object ): void {
+  const message = `data: ${ JSON.stringify( { event, ...data } ) }\n\n`;
+  clients.forEach( ( client, index ) => {
     try {
-      if (!client.writableEnded && !client.destroyed) {
-        client.write(message);
+      if ( !client.writableEnded && !client.destroyed ) {
+        client.write( message );
       } else {
         // Remove disconnected client
-        clients.splice(index, 1);
+        clients.splice( index, 1 );
       }
-    } catch (error) {
-      console.error('Error sending SSE:', error);
-      clients.splice(index, 1);
+    } catch ( error ) {
+      console.error( 'Error sending SSE:', error );
+      clients.splice( index, 1 );
     }
-  });
+  } );
 }
 
