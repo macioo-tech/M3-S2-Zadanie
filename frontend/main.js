c036f6cc50d599fe3a3cad3b2e6820851a9b84ae
mac <orno.morsoft@gmail.com>
2025-11-11 00:41:41 +0100

    corrected frontend to show owner_id

--- frontend/main.js
+++ frontend/main.js
@@ -140,7 +140,7 @@ async function loadCars() {
                      <strong>ID:</strong> ${car.id} |
                      <strong>Model:</strong> ${car.model} |
                      <strong>Cena:</strong> ${car.price} |
-                     <strong>Właściciel:</strong> ${car.ownerId}
+                     <strong>Właściciel:</strong> ${car.owner_id}
                    </div>`;
         });
       }
